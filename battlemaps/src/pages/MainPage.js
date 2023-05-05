8fa691db3779d760e02df2f4f5e12a955f4ed74d
jared <jaredmccallister93@gmail.com>
2023-05-05 17:31:14 -0400

    Wokring on getting a basemodel working

--- battlemaps/src/pages/MainPage.js
+++ battlemaps/src/pages/MainPage.js
@@ -1,10 +1,13 @@
+import BaseModel from "../components/BaseModel";
+
 const MainPage = (props) => {
   return (
     <>
       <h1>Welcome to Battlemaps</h1>
       <h2>Where you can generate Battlemaps based on your specific needs</h2>
-     <h1>Tune settings here</h1>
-     <h1>Image will display here</h1>
+      <h1>Tune settings here</h1>
+      <h1>Image will display here</h1>
+      <BaseModel />
     </>
   );
 };
