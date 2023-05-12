db316433a2dbe52ec2b1c971f235675241aaea9e
Jared <jaredmccallister93@gmail.com>
2023-05-12 12:46:42 -0700

    Wokring on getting assets

--- battlemaps/src/components/PhaserScene.js
+++ battlemaps/src/components/PhaserScene.js
@@ -1,30 +1,13 @@
 import React, { useRef, useEffect } from "react";
 import Phaser from "phaser";
 import p5 from "p5";
+import grass5 from "../assets/Grass/Grass_05-128x128.png"
+import grass8 from "../assets/Grass/Grass_08-128x128.png"
+import grass21 from "../assets/Grass/Grass_21-128x128.png"
+import dirt1 from "../assets/Dirt/Dirt_01-128x128.png"
+import dirt2 from "../assets/Dirt/Dirt_02-128x128.png"
+import dirt9  from "../assets/Dirt/Dirt_09-128x128.png"
 
-import dirt1Image from "../assets/dirt/Dirt 1 .png";
-import dirt2Image from "../assets/dirt/Dirt2.png";
-import dirt3Image from "../assets/dirt/Dirt 3 .png";
-import dirt4Image from "../assets/dirt/Dirt 4 .png";
-import dirt5Image from "../assets/dirt/Dirt 5 .png";
-import dirt6Image from "../assets/dirt/Dirt 6 .png";
-import dirt7Image from "../assets/dirt/Dirt 7 .png";
-import dirt8Image from "../assets/dirt/Dirt 8 .png";
-import dirt9Image from "../assets/dirt/Dirt 9 .png";
-import dirt10Image from "../assets/dirt/Dirt 10 .png";
-import dirt11Image from "../assets/dirt/dirt11.jpg";
-import grass1Image from "../assets/grass/grass1.png";
-import grass2Image from "../assets/grass/grass2.jpg";
-import grass3Image from "../assets/grass/grass3.jpg";
-import grass4Image from "../assets/grass/grass4.jpg";
-import grass5Image from "../assets/grass/grass5.png";
-import grass6Image from "../assets/grass/grass6.png";
-import grass7Image from "../assets/grass/grass7.png";
-import grass8Image from "../assets/grass/grass8.png";
-import grass9Image from "../assets/grass/grass9.png";
-
-import grass11Image from "../assets/grass/grass11.png";
-import grass12Image from "../assets/grass/grass12.png"
 
 const PhaserScene = (props) => {
   const phaserContainerRef = useRef(null);
@@ -37,29 +20,13 @@ const PhaserScene = (props) => {
       }
 
       preload() {
-        this.load.image("dirt1", dirt1Image);
-        this.load.image("dirt2", dirt2Image);
-        this.load.image("dirt3", dirt3Image);
-        this.load.image("dirt4", dirt4Image);
-        this.load.image("dirt5", dirt5Image);
-        this.load.image("dirt6", dirt6Image);
-        this.load.image("dirt7", dirt7Image);
-        this.load.image("dirt8", dirt8Image);
-        this.load.image("dirt9", dirt9Image);
-        this.load.image("dirt10", dirt10Image);
-        this.load.image("dirt11", dirt11Image);
-        this.load.image("grass1", grass1Image);
-        this.load.image("grass2", grass2Image);
-        this.load.image("grass3", grass3Image);
-        this.load.image("grass4", grass4Image);
-        this.load.image("grass5", grass5Image);
-        this.load.image("grass6", grass6Image);
-        this.load.image("grass7", grass7Image);
-        this.load.image("grass8", grass8Image);
-        this.load.image("grass9", grass9Image);
-     
-        this.load.image("grass11", grass11Image);
-        this.load.image("grass12", grass12Image);
+        this.load.image("dirt1", dirt1);
+        this.load.image("dirt2", dirt2);
+        this.load.image("dirt9", dirt9);
+        this.load.image("grass5", grass5);
+        this.load.image("grass8", grass8);
+        this.load.image("grass21", grass21);
+      
       }
 
       create() {
@@ -73,8 +40,9 @@ const PhaserScene = (props) => {
 
         //Set grass tiles arrays
         const grassTiles = [
-          "grass1",
-        //   "grass2",
+          "grass5",
+          "grass8",
+          "grass21"
         //   "grass3",
         //   "grass4",
         //   "grass5",
@@ -83,19 +51,20 @@ const PhaserScene = (props) => {
         //   "grass8",
         //   "grass9",
         //   "grass11",
-          "grass12"
+          // "grass12"
         ];
         const dirtTiles = [
           "dirt1",
           "dirt2",
-          "dirt3",
-          "dirt4",
-          "dirt5",
-          "dirt6",
-          "dirt7",
-          "dirt8",
-          "dirt9",
-          "dirt11",
+          "dirt9"
+          // "dirt3",
+          // "dirt4",
+          // "dirt5",
+          // "dirt6",
+          // "dirt7",
+          // "dirt8",
+          // "dirt9",
+          // "dirt11",
         ];
 
         const sketch = new p5();
