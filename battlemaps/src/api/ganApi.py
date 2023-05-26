6d8019c6c6523d84385be1320220a247ebe7c3f7
jaredmccallister93 <jaredmccallister93@gmail.com>
2023-05-26 12:44:32 -0700

    Starting over on linux

--- battlemaps/src/api/ganApi.py
+++ battlemaps/src/api/ganApi.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import torch
 import base64
 import io
 from PIL import Image
 from torchvision import transforms
+# from stylegan2_pytorch.model import Generator
+
+
+
 
 # Load model
 model = torch.hub.load('nvidia/StyleGAN', 'stylegan2')
 
 app = FastAPI()
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Allow all origins to make requests to this API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/generate/")
 async def generate_image(seed: int):
--- battlemaps/src/pages/MainPage.js
+++ battlemaps/src/pages/MainPage.js
@@ -1,17 +1,33 @@
 import s from "../style/main.module.scss";
-import BattleMapRender from "../components/BattleMapRender";
-import PhaserScene from "../components/PhaserScene";
+// import BattleMapRender from "../components/BattleMapRender";
+// import PhaserScene from "../components/PhaserScene";
+import axios from "axios";
+import { useState } from "react";
 
 const MainPage = (props) => {
+  const [imgSrc, setImgSrc] = useState(null);
+
+  const generateImage = async () => {
+    // Make a POST request to your FastAPI server
+    const response = await axios.post("http://localhost:8000/generate/");
+
+    // Get the base64 image from the response
+    const base64Image = response.data.image;
+
+    // Set the image source to be the base64 image
+    setImgSrc(`data:image/png;base64,${base64Image}`);
+  };
   return (
     <>
       <div className={s.mainPageContainer}>
         <h1>Welcome to Battlemaps</h1>
         <h2>Where you can generate Battlemaps based on your specific needs</h2>
-
- 
-          <PhaserScene />
-     
+        <div>
+          <button onClick={generateImage}>Generate Image</button>
+          {imgSrc && <img src={imgSrc} alt="Generated" />}
+        </div>
+        {/* <PhaserScene />
+         */}
       </div>
     </>
   );
